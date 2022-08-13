0ba43817062f8801e580b1a02890e7c1dadbf190
Alisher <umarovalisher132@gmail.com>
2022-08-13 14:09:07 +0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import routes from "./routes";
-import Layout from "./Components/Layout/Layout";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
