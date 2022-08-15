a601763c1c94ee6dc547a94e555eda11d1b7accf
Xurshid <nematoff4744@gmail.com>
2022-08-15 22:58:07 +0500

    update

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -11,8 +11,10 @@ const Product = () => {
   const { changeLanguage } = useSelector((state: any) => state.changeLanguge);
   const [data, setData] = useState([]);
   const [sortData, setSortData] = useState([]);
-  const name = data.map((item:any) => !changeLanguage ? item.name_uz : item.name_ru)[0]
-  
+  const name = data.map((item: any) =>
+    !changeLanguage ? item.name_uz : item.name_ru
+  )[0];
+
   const brends = Array.from(new Set(data.map((item: any) => item.brend)));
   const price = data
     .map((item: any) =>
@@ -28,11 +30,14 @@ const Product = () => {
   const [value, setValue] = useState(price[0]);
   console.log(price);
 
-  const range = data.filter(
-    (item: any) =>
-      Number(
-        item.price_ru.split("").slice(0, -5).join("").split(" ").join("")
-      ) >= value
+  const range = data.filter((item: any) =>
+    !changeLanguage
+      ? Number(
+          item.price_uz.split("").slice(0, -4).join("").split(" ").join("")
+        ) >= value
+      : Number(
+          item.price_ru.split("").slice(0, -5).join("").split(" ").join("")
+        ) >= value
   );
   console.log(range, "bu range");
 
@@ -46,72 +51,97 @@ const Product = () => {
   }, []);
   return (
     <section className="productContainer globalContainer">
-      <aside>
-        <h3>{!changeLanguage ? "Saralash" : "Фильтр"}</h3>
-        <h6>
-          {!changeLanguage ? "Narx bo'yicha saralash" : "Фильтрация по цене"}
-        </h6>
-        <span>
-          {value ? value.toLocaleString() : price[0]?.toLocaleString()}{" "}
-          {!changeLanguage ? "so'm" : "рубль"}
-        </span>{" "}
-        -{" "}
-        <span>
-          {price[price.length - 1]?.toLocaleString()}{" "}
-          {!changeLanguage ? "so'm" : "рубль"}
-        </span>
-        <Slider
-          defaultValue={0}
-          max={price[price.length - 1]}
-          min={price[0]}
-          onChange={(e) => setValue(e)}
-          tooltipVisible={false}
-          trackStyle={{ backgroundColor: "#1d1d1d" }}
-          handleStyle={{ borderColor: "#1d1d1d" }}
-          className="slider"
-        />
-        <h6>{!changeLanguage ? "Brendlar" : "Бренды"}</h6>
-        {brends?.map((item: any) => (
-          <p key={item} id={item} onClick={(e) => sort(e)}>
-            {item}
-          </p>
-        ))}
-      </aside>
-      <div className="productBox">
-        <h6>
-          {!changeLanguage ? "Bosh sahifa" : "Главная"} <b>{">"}</b> {name}
-        </h6>
-        <div className="titleBox">
-          <h3>{name}</h3>
+      {data.length > 0 ? (
+        <>
+          <aside>
+            <h3>{!changeLanguage ? "Saralash" : "Фильтр"}</h3>
+            <h6>
+              {!changeLanguage
+                ? "Narx bo'yicha saralash"
+                : "Фильтрация по цене"}
+            </h6>
+            <span>
+              {value ? value.toLocaleString() : price[0]?.toLocaleString()}{" "}
+              {!changeLanguage ? "so'm" : "рубль"}
+            </span>{" "}
+            -{" "}
+            <span>
+              {price[price.length - 1]?.toLocaleString()}{" "}
+              {!changeLanguage ? "so'm" : "рубль"}
+            </span>
+            <Slider
+              defaultValue={0}
+              max={price[price.length - 1]}
+              min={price[0]}
+              onChange={(e) => setValue(e)}
+              tooltipVisible={false}
+              trackStyle={{ backgroundColor: "#1d1d1d" }}
+              handleStyle={{ borderColor: "#1d1d1d" }}
+              className="slider"
+            />
+            <h6>{!changeLanguage ? "Brendlar" : "Бренды"}</h6>
+            {brends?.map((item: any) => (
+              <p key={item} id={item} onClick={(e) => sort(e)}>
+                {item}
+              </p>
+            ))}
+          </aside>
+          <div className="productBox">
+            <h6>
+              {!changeLanguage ? "Bosh sahifa" : "Главная"} <b>{">"}</b> {name}
+            </h6>
+            <div className="titleBox">
+              <h3>{name}</h3>
 
-          <Select defaultValue="All solutions" style={{ width: "200px" }}>
-            <Option value={"Xurshid"}>Xurshid</Option>
-          </Select>
-        </div>
-        <div className="cardContainer">
-          {sortData.length === 0
-            ? !value
-              ? data?.map((item: any) => (
-                  <BuyCard
-                    img={item.image}
-                    title={!changeLanguage ? item.model_uz : item.model_ru}
-                    price={!changeLanguage ? item.price_uz : item.price_ru}
-                    key={item.id}
-                  />
-                ))
-              : data
-                  ?.filter(
-                    (item: any) =>
-                      Number(
-                        item.price_ru
-                          .split("")
-                          .slice(0, -5)
-                          .join("")
-                          .split(" ")
-                          .join("")
-                      ) >= value
-                  )
-                  ?.map((item: any) => (
+              <Select defaultValue="All solutions" style={{ width: "200px" }}>
+                <Option value={"Xurshid"}>Xurshid</Option>
+              </Select>
+            </div>
+            <div className="cardContainer">
+              {sortData.length === 0
+                ? !value
+                  ? data?.map((item: any) => (
+                      <BuyCard
+                        img={item.image}
+                        title={!changeLanguage ? item.model_uz : item.model_ru}
+                        price={!changeLanguage ? item.price_uz : item.price_ru}
+                        key={item.id}
+                      />
+                    ))
+                  : data
+                      ?.filter((item: any) =>
+                        !changeLanguage
+                          ? Number(
+                              item.price_uz
+                                .split("")
+                                .slice(0, -4)
+                                .join("")
+                                .split(" ")
+                                .join("")
+                            ) >= value
+                          : Number(
+                              item.price_ru
+                                .split("")
+                                .slice(0, -5)
+                                .join("")
+                                .split(" ")
+                                .join("")
+                            ) >= value
+                      )
+                      ?.map((item: any) => (
+                        <BuyCard
+                          img={item.image}
+                          title={
+                            !changeLanguage ? item.model_uz : item.model_ru
+                          }
+                          price={
+                            !changeLanguage ? item.price_uz : item.price_ru
+                          }
+                          key={item.id}
+                        />
+                      ))
+                : !value
+                ? sortData?.map((item: any) => (
                     <BuyCard
                       img={item.image}
                       title={!changeLanguage ? item.model_uz : item.model_ru}
@@ -119,37 +149,40 @@ const Product = () => {
                       key={item.id}
                     />
                   ))
-            : !value
-            ? sortData?.map((item: any) => (
-                <BuyCard
-                  img={item.image}
-                  title={!changeLanguage ? item.model_uz : item.model_ru}
-                  price={!changeLanguage ? item.price_uz : item.price_ru}
-                  key={item.id}
-                />
-              ))
-            : sortData
-                ?.filter(
-                  (item: any) =>
-                    Number(
-                      item.price_ru
-                        .split("")
-                        .slice(0, -5)
-                        .join("")
-                        .split(" ")
-                        .join("")
-                    ) >= value
-                )
-                ?.map((item: any) => (
-                  <BuyCard
-                    img={item.image}
-                    title={!changeLanguage ? item.model_uz : item.model_ru}
-                    price={!changeLanguage ? item.price_uz : item.price_ru}
-                    key={item.id}
-                  />
-                ))}
-        </div>
-      </div>
+                : sortData
+                    ?.filter((item: any) =>
+                      !changeLanguage
+                        ? Number(
+                            item.price_uz
+                              .split("")
+                              .slice(0, -4)
+                              .join("")
+                              .split(" ")
+                              .join("")
+                          ) >= value
+                        : Number(
+                            item.price_ru
+                              .split("")
+                              .slice(0, -5)
+                              .join("")
+                              .split(" ")
+                              .join("")
+                          ) >= value
+                    )
+                    ?.map((item: any) => (
+                      <BuyCard
+                        img={item.image}
+                        title={!changeLanguage ? item.model_uz : item.model_ru}
+                        price={!changeLanguage ? item.price_uz : item.price_ru}
+                        key={item.id}
+                      />
+                    ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        "Loading...."
+      )}
     </section>
   );
 };
